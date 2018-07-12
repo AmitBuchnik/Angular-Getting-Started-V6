b672778b597c67fef6081a68c7242bca4b6bb41b
Amit Buhnik <P0020755@ness-tech.co.il>
2018-07-12 15:41:15 +0300

    hjk

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -9,7 +9,7 @@ export class StarComponent implements OnChanges {
     @Input() rating: number;
     starWidth: number;
     @Output() ratingClicked: EventEmitter<string> =
-            new EventEmitter<string>();
+        new EventEmitter<string>();
 
     ngOnChanges(): void {
         this.starWidth = this.rating * 70 / 5;
